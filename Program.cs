e805fc28e383877dbd09bfcf7b9db6b66d8109db
Hlusik <hlusik@gmail.com>
2019-08-20 12:42:09 +0300

    task1 and task2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Task1 t1 = new Task1.Task1();
+            t1.Execute();
+
+            Task2.Task2 t2 = new Task2.Task2();
+            t2.Execute();
+        }
+    }
+}
